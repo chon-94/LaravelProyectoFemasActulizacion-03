96120426d0ae26577930ae65c07e623a6106ad10
chon-94 <107455342+chon-94@users.noreply.github.com>
2026-03-13 22:24:16 -0500

    proyect

--- femas/app/Models/Project.php
+++ femas/app/Models/Project.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'location',
+        'year',
+        'category',
+        'link',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
